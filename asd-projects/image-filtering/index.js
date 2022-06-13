0a77029113f35d56092b722c0e508d4b15c170f8
HanhLe2005 <leemiha1219@gmail.com>
2022-06-13 01:08:35 +0000

    saving image-filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -21,6 +21,7 @@ function resetAndRender() {
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
 
+  applyFilter();
   
 
   // do not change the below line of code
@@ -33,6 +34,9 @@ function applyAndRender() {
 
 // TODO 1, 2 & 4: Create the applyFilter function here
 
+applyFilter(){
+
+}
 
 // TODO 7: Create the applyFilterNoBackground function
 
