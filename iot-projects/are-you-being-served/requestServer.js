5b016f3e1683664e1027ee5d9d9b5d98bf59ac1b
HanhLe2005 <leemiha1219@gmail.com>
2022-09-29 17:55:12 -0500

    are u being served

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,2 +1,11 @@
 // requestServer.js file
 const http = require("http");
+const request = require("request");
+
+const port = 1219;
+
+http.createServer(function(req, res){
+request()
+
+
+}).listen(port)
