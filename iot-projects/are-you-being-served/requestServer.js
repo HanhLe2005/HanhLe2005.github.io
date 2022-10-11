b897cdfb395fe534ba32659626a7d8e4dce122c5
HanhLe2005 <leemiha1219@gmail.com>
2022-10-11 17:29:45 -0500

    saving

--- iot-projects/are-you-being-served/requestServer.js
+++ iot-projects/are-you-being-served/requestServer.js
@@ -1,11 +1,29 @@
 // requestServer.js file
 const http = require("http");
 const request = require("request");
-
+var args = process.argv.slice(2);
 const port = 1219;
 
-http.createServer(function(req, res){
-request()
-
-
-}).listen(port)
+http
+  .createServer(function (req, res) {
+    var url = args[0] ? args[0] : "https://HanhLe2005.github.io";
+    request(url, function (error, response, body) {
+      if (
+        !body ||
+        !response ||
+        (error === null && response.statusCode !== 200)
+      ) {
+        res.end("bad URL\n");
+        return;
+      }
+      if (response.statusCode === 200 && !error === true) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(body);
+      } else {
+        res.writeHead(response.statusCode, { "Content-Type": "text/plain" });
+        res.write(error.toString());
+      }
+      res.end();
+    });
+  })
+  .listen(port);
--- test-server.js
+++ test-server.js
@@ -3,8 +3,6 @@ const port = process.argv[2];
 
 http.createServer(function (req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write("I want a ");
-  res.end("new Nintendo Switch");
   res.write("mcflurries are");
   res.end("the best");
 }).listen(port);
